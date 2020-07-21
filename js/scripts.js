c1a46a1f5994b15e9b9d438e64a1b15c0c6d03f2
szopiks@yahoo.com <szopiks@yahoo.com>
2020-07-21 14:56:17 -0700

    adding new file. Adding return button and fixing indentations

--- js/scripts.js
+++ js/scripts.js
@@ -1,81 +1,59 @@
 //Business Logic
 
 // Object literal
+
 const pizzaPrice = {
-    small: 10,
-    medium: 15,
-    large: 20,
+  small: 10,
+  medium: 15,
+  large: 20,
+};
 
-  };
-
-  const toppingsPrice = {
-    mushrooms : 1,
-   cheese: 1,
-   pepperoni: 1,
-   olives :2,
-   tomatoes:1,
-  };
+const toppingsPrice = {
+  mushrooms: 1,
+  cheese: 1,
+  pepperoni: 1,
+  olives: 2,
+  tomatoes: 1,
+};
 // Constructor
-function Pizza(size){
-    this.size = size;
-    this.topping =[];
-    this.total = pizzaPrice[size];
+
+function Pizza(size) {
+  this.size = size;
+  this.toppings = [];
+  this.total = pizzaPrice[size];
 }
- 
-Pizza.prototype.ShowTotal =function(toppings) {
- this.toppings.push(toppings);
- for(let i = 0; i < toppings.lenght; i++){
-     topping = toppings[i];
+Pizza.prototype.showTotal = function (toppings) {
+  this.toppings.push(toppings);
+  for (let i = 0; i < toppings.length; i++) {
+    topping = toppings[i];
 
-     if(Object.keys(toppingsPrice).includes(topping)){
-      this.total +=toppingsPrice[topping];  
-     }
- }
-return this.total;
-}
-
-  
-
-
-
+    if (Object.keys(toppingsPrice).includes(topping)) {
+      this.total += toppingsPrice[topping];
+    }
+  }
+  return this.total;
+};
 
 // UI logic
-    $(document).ready(function(){
-        console.log("hi");
-        $("form#pizza-parlor").submit(function(event){
-            event.preventDefault();
-            //getting the radiobutton value
-            
-            let size = $('input[name="size"]:checked').val();
-            console.log(size)
-            // getting the checkbox value
-           let toppings = $.map($('input[name="toppings"]:checked'), function (c) {
-            return c.value;
-          });
 
-           console.log(toppings)
- 
-           let pizza = new Pizza(size);
+$(document).ready(function () {
+  console.log("hi");
+  $("form#pizza-parlor").submit(function (event) {
+    event.preventDefault();
 
-           console.log( pizza.total)
-           
-        })
-      
-        
-    })
+    //getting the radiobutton value
 
-    // $("input[name=toppings]:checked").map(function() {
-    //     return this.value;
-    // }).get().join(",");
+    let size = $('input[name="size"]:checked').val();
 
-    
-    // function showSelectedValues()
-    // {
-    //   alert($("input[name="toppings"]:checked").map(
-    //      function () {return this.value;}).get().join(","));
-    // }
+    // getting the checkbox value
 
-     
+    let toppings = $.map($('input[name="toppings"]:checked'), function (c) {
+      return c.value;
+    });
 
+    let pizza = new Pizza(size);
+    let total = pizza.showTotal(toppings);
 
-
+   
+  });
+});
